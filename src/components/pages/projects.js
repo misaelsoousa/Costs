8eae0ebd1c9cb709a96ff61b95738b45389c0ff3
misaelsoousa <misasoousa@gmail.com>
2024-04-16 12:59:04 -0300

    Criação de mensagem de retorno ao criar um projeto

--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -1,4 +1,19 @@
-function Projetcs(){
-    return(<h1>Projetos</h1>)
+import Message from '../layout/message'
+
+import { useLocation } from 'react-router-dom'
+
+function Projetcs() {
+
+    const location = useLocation()
+    let message = ''
+    if(location.state){
+        message = location.state.message
+    }
+    return (
+        <div>
+            <h1>Meus Projetos</h1>
+            {message && <Message type="success" msg={message} />}
+        </div>
+    )
 }
 export default Projetcs
